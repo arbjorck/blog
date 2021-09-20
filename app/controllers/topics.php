dcd3045c2942a4819eab70951ac9024c3f54e875
arbjorck <arbjorck@gmail.com>
2021-09-20 16:04:41 +0200

    topics cc

--- app/controllers/topics.php
+++ app/controllers/topics.php
@@ -1,58 +1,58 @@
 <?php
-require(ROOT_PATH . "../../app/database/db.php");
-require(ROOT_PATH . "../../app/helpers/middleware.php");
-require(ROOT_PATH . "../../app/helpers/validateTopic.php");
+// require(ROOT_PATH . "../../app/database/db.php");
+// require(ROOT_PATH . "../../app/helpers/middleware.php");
+// require(ROOT_PATH . "../../app/helpers/validateTopic.php");
 
-class TopicsController
-{
-  private $dbModel;
+// class TopicsController
+// {
+//   private $dbModel;
 
-  function __construct()
-  {
-    $this->dbModel = new DbModel();
-  }
+//   function __construct()
+//   {
+//     $this->dbModel = new DbModel();
+//   }
 
-  public function getTopics($table)
-  {
-    // Création d'un objet
-    $topics = $this->dbModel->selectAll($table);
-    // Appel d'une fonction de cet objet
-    return $topics;
-  }
+//   public function getTopics($table)
+//   {
+//     // Création d'un objet
+//     $topics = $this->dbModel->selectAll($table);
+//     // Appel d'une fonction de cet objet
+//     return $topics;
+//   }
 
-  public function createTopic($table, $data)
-  {
-    $createTopic = $this->dbModel->create($table, $data);
+//   public function createTopic($table, $data)
+//   {
+//     $createTopic = $this->dbModel->create($table, $data);
 
-    $_SESSION['message'] = 'Le thème a été créé avec succès.';
-    $_SESSION['type'] = 'success';
-    header('location: ' . BASE_URL . '/views/admin/topics/index.php?admin=topics');
-    exit();
-  }
+//     $_SESSION['message'] = 'Le thème a été créé avec succès.';
+//     $_SESSION['type'] = 'success';
+//     header('location: ' . BASE_URL . '/views/admin/topics/index.php?admin=topics');
+//     exit();
+//   }
 
-  public function selectOneTopic($table, $data)
-  {
-    $selectOne = $this->dbModel->selectOne($table, $data);
-    return $selectOne;
-  }
+//   public function selectOneTopic($table, $data)
+//   {
+//     $selectOne = $this->dbModel->selectOne($table, $data);
+//     return $selectOne;
+//   }
 
-  public function deleteTopic($table, $data)
-  {
-    $deleteTopic = $this->dbModel->delete($table, $data);
+//   public function deleteTopic($table, $data)
+//   {
+//     $deleteTopic = $this->dbModel->delete($table, $data);
 
-    $_SESSION['message'] = 'Thème suprimé avec succès';
-    $_SESSION['type'] = 'success';
-    header('location: ' . BASE_URL . '/views/admin/topics/index.php?admin=topics');
-    exit();
-  }
+//     $_SESSION['message'] = 'Thème suprimé avec succès';
+//     $_SESSION['type'] = 'success';
+//     header('location: ' . BASE_URL . '/views/admin/topics/index.php?admin=topics');
+//     exit();
+//   }
 
-  public function updateTopic($table, $id, $data)
-  {
-    $selectOne = $this->dbModel->update($table, $id, $data);
+//   public function updateTopic($table, $id, $data)
+//   {
+//     $selectOne = $this->dbModel->update($table, $id, $data);
 
-    $_SESSION['message'] = 'Thème actualisé avec succès';
-    $_SESSION['type'] = 'success';
-    header('location: ' . BASE_URL . '/views/admin/topics/index.php?admin=topics');
-    exit();
-  }
-}
+//     $_SESSION['message'] = 'Thème actualisé avec succès';
+//     $_SESSION['type'] = 'success';
+//     header('location: ' . BASE_URL . '/views/admin/topics/index.php?admin=topics');
+//     exit();
+//   }
+// }
