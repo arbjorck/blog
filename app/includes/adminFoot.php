778197d800a3ee3f64d37918b996bff6e2f8bfbe
arbjorck <arbjorck@gmail.com>
2021-09-20 20:15:52 +0200

    includes

---
+++ app/includes/adminFoot.php
@@ -0,0 +1,8 @@
+<!-- JQuery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+<!-- Ckeditor -->
+<script src="https://cdn.ckeditor.com/ckeditor5/29.0.0/classic/ckeditor.js"></script>
+
+<!-- Custom Script -->
+<script src="../../../assets/js/scripts.js"></script>
---
+++ app/includes/adminHead.php
@@ -0,0 +1,18 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Font awesome -->
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+<!-- Google Font -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+<!-- Custom Styling -->
+<link rel="stylesheet" href="../../../assets/css/style.css">
+<link rel="stylesheet" href="../../../assets/css/style_media.css">
+
+<!-- Admin Styling -->
+<link rel="stylesheet" href="../../../assets/css/admin.css"> 
+<link rel="stylesheet" href="../../../assets/css/admin_media.css">
