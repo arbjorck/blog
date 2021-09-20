0c44dd012912a1774092feb0e3dfca2e45e8f3c7
arbjorck <arbjorck@gmail.com>
2021-09-20 16:05:34 +0200

    middleware cc

--- app/helpers/middleware.php
+++ app/helpers/middleware.php
@@ -2,16 +2,6 @@
 
 class Middleware
 {
-    public function usersOnly() 
-    {
-        if (empty($_SESSION ['id'])) {
-            $_SESSION['message'] = 'Une authentification est requise.';
-            $_SESSION['type'] = 'error';
-            header("location: " . BASE_URL . "/views/public/single.php?id=" . $_GET['id']);
-            exit(0);
-        }
-    }
-
     public function adminOnly($redirect = '') 
     {
         if (empty($_SESSION ['id']) || empty($_SESSION ['admin'])) {
